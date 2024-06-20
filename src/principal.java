df2babcae824a21b1d7c3cf882d3a184cc2e7866
chris <ngalatshi243@gmail.com>
2024-06-20 16:40:49 -0300

    adicionando arquivo

---
+++ src/principal.java
@@ -0,0 +1,13 @@
+
+public class principal {
+	public static void main(String[] args) {
+		
+		iPhone celular = new iPhone();
+		
+		celular.ligar("51991745227");
+		celular.atender();
+		celular.selecionarMusica("beautiful thing");
+		celular.pausar();
+		celular.exibirPagina("www.java.com");
+	}
+}
